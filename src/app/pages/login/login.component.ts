36e8016ce0e2f0e59abe8705de21057af2698102
Anderson Pedro <anderson.nascimento.017@ufrn.edu.br>
2025-12-17 16:08:31 -0300

    Criacao component home

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validator, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+ name = new FormControl
+}
