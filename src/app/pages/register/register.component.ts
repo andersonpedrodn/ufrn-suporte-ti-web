1e67aae8f04b581a54aea9b6473940c1ac4c649c
Anderson Pedro do Nascimento <andersonpedrodn@gmail.com>
2025-12-19 10:59:14 -0300

    feat: aplica identidade visual Potiguana TI, fonte Space Grotesk e novo layout de login/cadastro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss',
+  styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', [Validators.required]], // Novo campo: Cargo
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      terms: [false, [Validators.requiredTrue]] // Novo campo: Termos (Obrigatório ser true)
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
+        alert('ERRO: As senhas não conferem!');
+        return;
+      }
+      console.log('Dados do formulário:', this.registerForm.value);
+      alert('Conta criada com sucesso! (Simulação)');
+      this.router.navigate(['/login']);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
